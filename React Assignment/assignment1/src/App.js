2ac05680612262387686a1e713ea48efc3320f9b
sanyamsri <sanyam.srivastava@sprinklr.com>
2022-05-31 13:55:50 +0530

    feat(color picker):Added the component for color picker
    installed the color picker library and added it as a component to the assignment

--- React Assignment/assignment1/src/App.js
+++ React Assignment/assignment1/src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
-
+import { React, useState } from "react";
+import { CirclePicker } from "react-color";
 function App() {
-  return <div className="App"></div>;
+  const [displayColorPicker, setDisplayColorPicker] = useState(false);
+
+  const handleClick = () => {
+    setDisplayColorPicker((prevState) => !prevState);
+  };
+  const handleChangeComplete = (color) => {
+    console.log(color.hex);
+  };
+
+  return (
+    <div className="App">
+      <button onClick={handleClick}>Pick Color</button>
+      {displayColorPicker ? (
+        <CirclePicker onChangeComplete={handleChangeComplete} />
+      ) : null}
+    </div>
+  );
 }
 
 export default App;
